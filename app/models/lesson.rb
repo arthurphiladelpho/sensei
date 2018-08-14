be100215c26b808bd771736a4e9a63b7b63c0af4
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-14 12:39:44 -0300

    5 unique Lessons created in seeds

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,10 @@
 class Lesson < ApplicationRecord
+  has_many :bookings
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :difficulty, inclusion: { in: [1, 2, 3] }
+  validates :duration, presence: true
+  validates :price, presence: true
+  validates :style, presence: true
 end
