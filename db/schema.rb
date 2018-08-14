250f3532d5dc8974d711b6991c445a0a8ea722d1
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-14 12:09:56 -0300

    lesson model added and migtrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_08_14_145732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "style"
+    t.integer "difficulty"
+    t.integer "duration"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
