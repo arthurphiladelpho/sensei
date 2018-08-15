4ae74cedb755321c2adf62bc2a0507424e49c070
Arthur Philadelpho <arthur22rj@gmail.com>
2018-08-15 15:19:36 -0300

    Minor fixes on Lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,10 +1,7 @@
 class Lesson < ApplicationRecord
   has_many :bookings
-
-  # Somebody, please, check this. I am not sure
   has_many :users, through: :bookings
-
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :description, presence: true
   validates :difficulty, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :duration, presence: true
