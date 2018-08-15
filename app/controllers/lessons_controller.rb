ad30b0ded22d73f8c6b77f66e656ad3044674d78
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-15 14:17:27 -0300

    inserted map into lessons index and show

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,18 @@ class LessonsController < ApplicationController
 
 
   def index
+
     @lessons = Lesson.all
+
+    # decided NOT to use the line below as we want ALL flats displayed on the page, regardless of whether they have lat/long.  This is due to API limits potentially not showing anything.  For deployment we would use the below line.
+    # @lessons = Lesson.where.not(latitude: nil, longitude: nil)
+    # create markers from the lesson latitude and longitude attributes which will be available to be displayed on the map.
+    @markers = @lessons.map do |lesson|
+      {
+        lat: lesson.latitude,
+        lng: lesson.longitude
+      }
+    end
   end
 
   def show
