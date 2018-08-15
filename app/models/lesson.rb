3877a6a47b9b9af735f4327aeb5cb9dabe7721fa
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-15 12:03:03 -0300

    links working in Lessons pages and Navbar link fixed

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,7 +3,7 @@ class Lesson < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :difficulty, presence: true, inclusion: { in: [1, 2, 3. 4, 5] }
+  validates :difficulty, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :duration, presence: true
   validates :price, presence: true
   validates :style, presence: true
