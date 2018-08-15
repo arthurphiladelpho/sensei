ed68a5609bdf7195f75b11bc2436b1f34061d350
Eleonora Kiziv <eleonora_kiziv@brown.edu>
2018-08-15 12:20:26 -0300

    Add validations to booking, lesson and user models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
+
+  #checking that associated objects (user and lesson are present)
+  validates :user, presence: true
+  validates :lesson, presence: true
+
+  validates :datetime, presence: true
 end
