bda4e162e687643b545ad4f0f0d1e72d382217f9
Steve Knight <stevejknight@hotmail.co.uk>
2018-08-15 11:35:16 -0300

    fixes bug in Lesson Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_14_190156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "lessons", force: :cascade do |t|
     t.string "name"
@@ -46,5 +44,5 @@ ActiveRecord::Schema.define(version: 2018_08_14_190156) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-end
 
+end
