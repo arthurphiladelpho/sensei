68a020cd1a70b82f74dd9424f68dc0425b094015
Arthur Philadelpho <arthur22rj@gmail.com>
2018-08-16 13:58:16 -0300

    Rolled back AddLessonToUser migration and ran AddUserToLessons migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_150017) do
+ActiveRecord::Schema.define(version: 2018_08_15_161801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,13 +54,10 @@ ActiveRecord::Schema.define(version: 2018_08_16_150017) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.bigint "lesson_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["lesson_id"], name: "index_users_on_lesson_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "lessons"
   add_foreign_key "bookings", "users"
-  add_foreign_key "users", "lessons"
 end
