eb06a97a68cc2db824917ac4f4ef33ab17cda32a
Arthur Philadelpho <arthur22rj@gmail.com>
2018-08-16 17:39:52 -0300

    One more time, removed lesson_id from users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_08_16_185526) do
 
   # These are extensions that must be enabled in order to support this database
@@ -58,14 +57,11 @@ ActiveRecord::Schema.define(version: 2018_08_16_185526) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.bigint "lesson_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["lesson_id"], name: "index_users_on_lesson_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "lessons"
   add_foreign_key "bookings", "users"
   add_foreign_key "lessons", "users"
-
 end
