d68cee4db8d979974d64b065a3f3eb1bc7b9100d
sachi <sachi1210@me.com>
2018-08-16 16:53:45 -0300

    Create lesson new page and Add new and create method in lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -25,4 +25,33 @@ class LessonsController < ApplicationController
     }]
 
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.user = current_user
+
+    if @lesson.save
+      redirect_to @lesson
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(
+      :name,
+      :description,
+      :style,
+      :difficulty,
+      :duration,
+      :price,
+      :location
+    )
+  end
 end
